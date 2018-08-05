fbe88df34c4db2f3bd374d56d741fe56dcd01bd0
jcosio <jcosio@calpoly.edu>
2018-08-05 12:09:52 -0700

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,39 @@
+var navigationItems = {
+  "cities": [
+    {
+      "section": "cupertino",
+      "label": "Cupertino",
+      "GMT": "5",
+    },
+    {
+      "section": "new-york-city",
+      "label": "New York City",
+      "GMT": "8",
+    },
+    {
+      "section": "london",
+      "label": "London",
+      "GMT": "1",
+    },
+    {
+      "section": "amsterdam",
+      "label": "Amsterdam",
+      "GMT": "2",
+    },
+    {
+      "section": "tokyo",
+      "label": "Tokyo",
+      "GMT": "9",
+    },
+    {
+      "section": "hong-kong",
+      "label": "Hong Kong",
+      "GMT": "8",
+    },
+    {
+      "section": "sydney",
+      "label": "Sydney",
+      "GMT": "10",
+    }
+  ]
+};
